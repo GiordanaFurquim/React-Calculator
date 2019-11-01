317218e295f3832668728ae70eff106fa21721c0
Giordana Furquim <giordanafurquim@gmail.com>
2019-11-01 16:28:14 +0100

    Part One Complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
+import ClearButton from './components/ClearButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: "",
+            previousNumber: "",
+            currentNumber: "",
+            operator: ""
+        };
+    }
+
+    addToInput = val => {
+        this.setState({ input: this.state.input + val });
+    };
+
+
+    render() {
+        return (
+          <div className="App">
+          <div className="calc-wrapper">
+                <div className="row">
+                <Input>{this.state.input}</Input>
+                </div>
+              <div className="row">
+                  <Button handleClick={this.addToInput}>7</Button>
+                  <Button handleClick={this.addToInput}>8</Button>
+                  <Button handleClick={this.addToInput}>9</Button>
+                  <Button handleClick={this.addToInput}>/</Button>
+              </div>
+              <div className="row">
+                  <Button handleClick={this.addToInput}>4</Button>
+                  <Button handleClick={this.addToInput}>5</Button>
+                  <Button handleClick={this.addToInput}>6</Button>
+                  <Button handleClick={this.addToInput}>*</Button>
+              </div>
+              <div className="row">
+                  <Button handleClick={this.addToInput}>1</Button>
+                  <Button handleClick={this.addToInput}>2</Button>
+                  <Button handleClick={this.addToInput}>3</Button>
+                  <Button handleClick={this.addToInput}>+</Button>
+              </div>
+              <div className="row">
+                  <Button handleClick={this.addToInput}>.</Button>
+                  <Button handleClick={this.addToInput}>0</Button>
+                  <Button handleClick={this.addToInput}>=</Button>
+                  <Button handleClick={this.addToInput}>-</Button>
+                  </div>
+                  <div className="row">
+                  <ClearButton>Clear</ClearButton>
+                  </div>
+              </div>
+          </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/ClearButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ClearButton.css';
+
+class ClearButton extends Component {
+    render() {
+        return (
+          <div className="clear-btn">
+            {this.props.children}
+          </div>
+        );
+    }
+}
+
+export default ClearButton;
